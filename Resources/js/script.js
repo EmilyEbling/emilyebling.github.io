b7c06b08029649dd35a4460f34541c7ccca506c6
Emily Ebling <eblinge@etown.edu>
2019-01-28 20:46:36 -0500

    Adds responsiveness

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -27,4 +27,40 @@ $(document).ready(function () {
         offset: '95%'
     });
 
+    $('.jsNavIcon').click(function() {
+       var nav = $('.jsMainNav');
+       var icon = $('.jsNavIcon i');
+
+        nav.slideToggle(200);
+
+        if (icon.hasClass('ion-navicon')) {
+           icon.addClass('ion-close');
+           icon.removeClass('ion-navicon');
+       }
+       else {
+           icon.addClass('ion-navicon');
+           icon.removeClass('ion-close');
+       }
+
+    });
+
+    $(window).resize(function(){
+
+        var nav = $('.jsMainNav');
+
+        var icon = $('.jsNavIcon i');
+
+        if ($(window).width() > 767){
+            nav.css("display", "block");
+            icon.addClass('ion-close');
+            icon.removeClass('ion-navicon');
+        }
+        else {
+            nav.css("display", "none");
+            icon.addClass('ion-navicon');
+            icon.removeClass('ion-close');
+        }
+
+    });
+
 });
